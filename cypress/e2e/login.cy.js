88518342065bab3a39745a6b562fcb1c6f2f7240
Carlos Henrryque <kaike.ch03@gmail.com>
2024-01-10 21:38:19 -0300

    teste commit

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,7 +1,7 @@
 /// <reference types="cypress" />
 describe('Login - Testes de API ServRest', () => {
 
-  it('Deve fazer login com sucesso', () => {
+  it.only('Deve fazer login com sucesso', () => {
     cy.request({
       method: 'POST',
       url: 'login',
